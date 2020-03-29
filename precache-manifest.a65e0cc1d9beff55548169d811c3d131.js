5432a751fb104698b11d1e4e7a5e0b431f8d6ec5
Sadanand Pai <Sadanand.Pai@trelleborg.com>
2020-03-29 16:39:17 +0530

    Modified homepage path

---
+++ precache-manifest.a65e0cc1d9beff55548169d811c3d131.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "484cb8bc69de4403cc7c36ab1fd2d751",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "8f86b248ff2ef505d15f",
+    "url": "/covid19/static/css/main.1287ef76.chunk.css"
+  },
+  {
+    "revision": "b0785eb2bca3b23b8dd1",
+    "url": "/covid19/static/js/2.66bd1869.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/covid19/static/js/2.66bd1869.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f86b248ff2ef505d15f",
+    "url": "/covid19/static/js/main.30356d16.chunk.js"
+  },
+  {
+    "revision": "9e2a371a0e75d5611f2e",
+    "url": "/covid19/static/js/runtime-main.eee28b2b.js"
+  }
+]);
