e5bf479d2148cbea62a786bda3ff824e73dfaa15
Sadanand Pai <Sadanand.Pai@trelleborg.com>
2020-03-29 16:30:37 +0530

    Initial commit

---
+++ precache-manifest.701d27b373e434d2c6156ca681805baa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29a05180660e98bf185f54b0bade8332",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f5c84b07e3100c9efda",
+    "url": "/static/css/main.1287ef76.chunk.css"
+  },
+  {
+    "revision": "b0785eb2bca3b23b8dd1",
+    "url": "/static/js/2.66bd1869.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.66bd1869.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f5c84b07e3100c9efda",
+    "url": "/static/js/main.769bbc8c.chunk.js"
+  },
+  {
+    "revision": "b777e1e5607df564d26b",
+    "url": "/static/js/runtime-main.aaf88d20.js"
+  }
+]);
